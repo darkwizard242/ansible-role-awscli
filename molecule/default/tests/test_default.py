9f96423d612f9ac371414bba9965590a57252af6
darkwizard242 <am900820@gmail.com>
2020-11-22 15:36:01 -0500

    testinfra functions refactored to use vars and desc

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,13 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE_BINARY = '/usr/local/bin/awscli'
+
+
 def test_awscli_binary_exists(host):
-    assert host.file('/usr/local/bin/aws').exists
+    """
+    Tests if awscli binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_awscli_binary_file(host):
-    assert host.file('/usr/local/bin/aws').is_file
+    """
+    Tests if awscli binary is a file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_awscli_binary_which(host):
-    assert host.check_output('which aws') == '/usr/local/bin/aws'
+    """
+    Tests the output to confirm awscli's binary location.
+    """
+    assert host.check_output('which aws') == PACKAGE_BINARY
