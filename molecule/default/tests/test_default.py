9f720607f3a997efc62662a11f9277c9ba8a3c05
darkwizard242 <am900820@gmail.com>
2020-11-22 15:40:59 -0500

    testinfra functions - corrected binary name

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,25 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-PACKAGE_BINARY = '/usr/local/bin/awscli'
+PACKAGE_BINARY = '/usr/local/bin/aws'
 
 
-def test_awscli_binary_exists(host):
+def test_aws_binary_exists(host):
     """
-    Tests if awscli binary exists.
+    Tests if aws binary exists.
     """
     assert host.file(PACKAGE_BINARY).exists
 
 
-def test_awscli_binary_file(host):
+def test_aws_binary_file(host):
     """
-    Tests if awscli binary is a file type.
+    Tests if aws binary is a file type.
     """
     assert host.file(PACKAGE_BINARY).is_file
 
 
-def test_awscli_binary_which(host):
+def test_aws_binary_which(host):
     """
-    Tests the output to confirm awscli's binary location.
+    Tests the output to confirm aws's binary location.
     """
     assert host.check_output('which aws') == PACKAGE_BINARY
